ae60ddeca684638ef303f500f81027ee18006101
achdaisy <oduordaizy@gmail.com>
2024-09-05 23:57:26 +0300

    More components and props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Gallery from './Gallery.js'
+import {Profile} from './Gallery.js'
 
 function AboutPage(){
   return(
@@ -18,7 +19,52 @@ function MyButton(){
   )
 }
 
+//dynamically specifying attributes within the JSX file
+function Avatar(){
+  const avatar = "https://i.imgur.com/7vQD0fPs.jpg";
+  const description = "Gregorio Y. Zara";
 
+  return(
+    <img
+      className="avatar"
+      src={avatar}
+      alt={description}
+    />
+
+  )
+
+}
+
+function ToDoList(){
+  const name = "Daisy";
+  return(
+    <p>This is {name}'s To Do List</p>
+  )
+}
+
+const today = new Date();
+
+function FormatDate(date){
+  return new Intl.DateTimeFormat(
+    'en-US',
+    {weekday:'long'}
+  ).format(date)
+
+}
+
+function NewToDoList(){
+  return (
+    <ul style={{ 
+      backgroundColor: "#363737 ",
+      color: 'white'   
+    }}>
+      <li>Improve the videophone</li>
+      <li>Prepare aeronautics lectures</li>
+      <li>Work on the alcohol fuelled engine</li>
+
+    </ul>
+  )
+}
 
 function App(){
   return (
@@ -27,6 +73,11 @@ function App(){
       <AboutPage></AboutPage>
       <MyButton></MyButton>
       <Gallery></Gallery>
+      <h1>Avatar dynamic image</h1>
+      <Avatar></Avatar>
+      <ToDoList></ToDoList>
+      <h1> To Do list for {FormatDate(today)}</h1>
+      <NewToDoList></NewToDoList>
     </div>
   )
 }
---
+++ src/props.js
@@ -0,0 +1,9 @@
+function Avatar({person, size}){
+    return (
+        <img
+            className="avatar"
+            
+
+    )
+
+}
